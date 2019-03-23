d6d18e18f9783199a42095320771ed0c636481b9
Bestony <best.tony@foxmail.com>
2019-03-24 00:22:47 +0800

    feat: nav bar

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,31 @@
       <Description></Description>
     </div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="menu">
+        <router-link to="/" class="pb">
+          <span class="prefix">Porn</span>
+          <span class="postfix">Hub</span>
+        </router-link>
+        <span class="disable yb">
+          <span class="prefix">You</span>  
+          <span class="postfix">tube</span>
+        </span>
+        <span class="disable">More in Progress...</span>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Logo from './components/Logo';
-import Description from './components/Description'
+import Logo from "./components/Logo";
+import Description from "./components/Description";
 export default {
-  components:{
-    logo:Logo,
+  components: {
+    logo: Logo,
     Description
   }
-}
+};
 </script>
 
 <style>
@@ -31,14 +40,56 @@ body,
 #app {
   height: 100%;
   background-color: #000;
-  color:#f90;
+  color: #f90;
 }
-a{
-  color:#f90;
+a {
+  color: #f90;
+  text-decoration: none;
 }
-.logo,.description {
+.logo,
+.description {
   display: flex;
   flex-direction: row;
   justify-content: center;
 }
+#nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding-top: 20px;
+}
+.menu {
+  width: 60%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  font-weight: 700;
+}
+.pb .prefix {
+  color: #fff;
+  padding: 2px;
+}
+.pb .postfix {
+  color: #000;
+  background-color: #f90;
+  padding: 2px;
+  border-radius: 3px;
+}
+.yb .prefix{
+  color: #fff;
+  padding: 2px;
+}
+.yb .postfix{
+  color: #fff;
+  background-color: #ff0000;
+  padding: 2px;
+  border-radius: 3px;
+  letter-spacing: -1px;
+}
+span.disable{
+  color:#ccc;
+  font-weight: 500;
+}
+
 </style>
