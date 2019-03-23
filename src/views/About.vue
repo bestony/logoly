c4cb015cdbf91c87f79a5ccf029f468b0d8821a4
Bestony <best.tony@foxmail.com>
2019-03-24 03:12:14 +0800

    feat: add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>Logoly.pro</h3>
+    <p>This Project is Opensource Project, you can find it on <a href="https://github.com/bestony/logoly" target="_blank"> Github</a></p>
   </div>
 </template>
+<style lang="stylus" scoped>
+p
+  color #fff
+</style>
