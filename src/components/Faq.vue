f93c6c17b8e16f04691357a3570fefa7ecb123be
Bestony <best.tony@foxmail.com>
2019-03-25 21:14:35 +0800

    feat: add copyright infomation , fixed #11

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -12,6 +12,10 @@ li
                     <p>Q: How to use this generator?</p>
                     <p>A: Edit the text in box, then click the `Export` button.</p>
                 </li>
+                <li>
+                    <p>Q: What Copyright of the file ? </p>
+                    <p>A: The File Copyright is belong to You.</p>
+                </li>
             </ol>
         </p>
     </div>
