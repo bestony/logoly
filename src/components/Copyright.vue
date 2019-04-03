e3c3af9118cabc780f06ada6643077f69292658d
Bestony <best.tony@foxmail.com>
2019-04-03 22:01:57 +0800

    feat: delete old code

--- src/components/Copyright.vue
+++
@@ -1,9 +0,0 @@
-<style lang="stylus" scoped>
-p
-    text-align center
-    color #666
-</style>
-
-<template>
-    <p>© Bestony 2019 <a href="https://bitbear.net/?refcode=1ndyezv73" target="_blank">比特熊,微服务</a></p>
-</template>
--- src/router.js
+++ src/router.js
@@ -4,53 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'pornhub',
-      component: () => import(/* webpackChunkName: "pornhub" */ './generator/Pornhub.vue'),
-      meta: {
-        analytics: {
-          pageviewTemplate(route) {
-            return {
-              title: 'Pornhub Generator',
-              page: route.path,
-            }
-          },
-        },
-      },
-    },
-    {
-      path: '/vertical-ph',
-      name: 'vertical-pornhub',
-      component: () =>
-        import(/* webpackChunkName: "vertical-pornhub" */ './generator/VerticalPornHub.vue'),
-      meta: {
-        analytics: {
-          pageviewTemplate(route) {
-            return {
-              title: 'VerticalPornHub Generator',
-              page: route.path,
-            }
-          },
-        },
-      },
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-      meta: {
-        analytics: {
-          pageviewTemplate(route) {
-            return {
-              title: 'About',
-              page: route.path,
-            }
-          },
-        },
-      },
-    },
   ],
 })
