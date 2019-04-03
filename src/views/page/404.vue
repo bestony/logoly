7296f1532c28b6ad39340c9fc900814b7b20315d
Bestony <best.tony@foxmail.com>
2019-04-04 00:19:25 +0800

    feat: add link to 404

--- src/views/page/404.vue
+++ src/views/page/404.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="notfound">
         <h1>404 Not Found</h1>
+        <h2>maybe you want <router-link to="/pornhub">Pornhub Flavour Logo>></router-link></h2>
     </div>
 </template>
 <style lang="stylus" scoped>
