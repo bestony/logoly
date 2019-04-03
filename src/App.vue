35e166c6fecad681749186be9e95a00c589f8c3b
Bestony <best.tony@foxmail.com>
2019-04-03 22:58:33 +0800

    feat: move some code from index to app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo">
+                        <span class="logoly">Logoly</span>
+                        <span class="pro">Pro</span>
+                    </div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            Home
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            FAQ
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            About
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Content :style="{padding: '50px 50px'}">
+
+               <router-view></router-view>
+               
+            </Content>
+            <Footer class="layout-footer-center">2019 &copy; Bestony</Footer>
+        </Layout>
+    </div>
   </div>
 </template>
 
@@ -17,5 +48,28 @@ export default {
 <style lang="stylus" scoped>
 html,#app,body
   height 100%
+
+.layout
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+
+.layout-logo
+    width: 100px;
+    height: 30px;
+    color: #fff;
+    float: left;
+    position: relative;
+    font-size 20px
+
+.layout-nav
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+
+.layout-footer-center
+    text-align: center;
 
 </style>
--- src/views/page/Index.vue
+++ src/views/page/Index.vue
@@ -1,23 +1,5 @@
 <style scoped lang="stylus">
-.layout
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
 
-.layout-logo
-    width: 100px;
-    height: 30px;
-    color: #fff;
-    float: left;
-    position: relative;
-    font-size 20px
-
-.layout-nav
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
 
 .title
     margin 50px 0px 20px 0px
@@ -40,38 +22,10 @@
         .sponsor
             height 50px
 
-.layout-footer-center
-    text-align: center;
-
 </style>
 <template>
-    <div class="layout">
-        <Layout>
-            <Header>
-                <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo">
-                        <span class="logoly">Logoly</span>
-                        <span class="pro">Pro</span>
-                    </div>
-                    <div class="layout-nav">
-                        <MenuItem name="1">
-                            <Icon type="ios-navigate"></Icon>
-                            Home
-                        </MenuItem>
-                        <MenuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            FAQ
-                        </MenuItem>
-                        <MenuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            About
-                        </MenuItem>
-                    </div>
-                </Menu>
-            </Header>
-            <Content :style="{padding: '50px 50px'}">
-
-               <IndexCarousel></IndexCarousel>
+    <div class="index">
+        <IndexCarousel></IndexCarousel>
                <div class="title">
                    <h1>Logos</h1>
                </div>
@@ -103,9 +57,6 @@
                         </Col>
                     </Row>
                </div>
-            </Content>
-            <Footer class="layout-footer-center">2019 &copy; Bestony</Footer>
-        </Layout>
     </div>
 </template>
 <script>
