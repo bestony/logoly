e8118e1524daa8ad2b83d74cc769a6f9ea8230fc
Bestony <best.tony@foxmail.com>
2019-04-03 23:46:57 +0800

    feat: delete iconFont

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import VueAnalytics from 'vue-analytics'
 import VTooltip from 'v-tooltip'
 
-import './assets/iconfont/iconfont.css'
 import store from './store'
 import './plugins/iview.js'
 
