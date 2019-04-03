8e4854fe5004ee562dac2b1aeb67ffdc44763306
Bestony <best.tony@foxmail.com>
2019-04-04 00:06:32 +0800

    feat: add icon to faq

--- src/views/page/Faq.vue
+++ src/views/page/Faq.vue
@@ -6,18 +6,18 @@
 </style>
 
 <template>
-    <div class="faq" icon="md-help" shadow>
+    <div class="faq" shadow>
         <h1 class="title">FAQ</h1>
         <Card class="faq-box">
-            <p slot="title">How to use this generator?</p>
+            <p slot="title"><Icon type="md-help"></Icon> How to use this generator?</p>
             <p>The generator is very simple to use. You can get a logo in just 4 steps:</p>
             <p>1. Choose a style (e.g. Horizontal/Vertical, Pornhub/Youtube/...).</p>
             <p>2. Edit the text in the center box.</p>
             <p>3. Customize your logo (color, background, font size, etc.).</p>
             <p>4. Finally, click the "Export" button and get the logo of your own.</p>
         </Card>
-        <Card class="faq-box">
-            <p slot="title">Can I use the generated logo for personal or commercial purposes?</p>
+        <Card class="faq-box" icon="md-help">
+            <p slot="title"><Icon type="md-help"></Icon> Can I use the generated logo for personal or commercial purposes?</p>
             <p>The generated logo fully belongs to you. You can use it freely for any purposes. Though credit is always appreciated.</p>
         </Card>
     </div>
