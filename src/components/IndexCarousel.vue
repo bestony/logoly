53b3a404b80c31e0f2c15d10324331ee56acbd98
Bestony <best.tony@foxmail.com>
2019-04-03 23:12:35 +0800

    feat: add gradient to carousel

--- src/components/IndexCarousel.vue
+++ src/components/IndexCarousel.vue
@@ -13,22 +13,22 @@
 <template>
     <Carousel v-model="value1" loop>
         <CarouselItem>
-            <div class="demo-carousel">
+            <div class="demo-carousel" style="background-image: linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%);">
                 Logoly —— A Simple Logo generator
             </div>
         </CarouselItem>
         <CarouselItem>
-            <div class="demo-carousel">
+            <div class="demo-carousel" style="background-image: linear-gradient( 135deg, #5EFCE8 10%, #736EFE 100%);">
                 Just need 3 seconds
             </div>
         </CarouselItem>
         <CarouselItem>
-            <div class="demo-carousel">
+            <div class="demo-carousel" style="background-image: linear-gradient( 135deg, #52E5E7 10%, #130CB7 100%);">
                 Choose, Edit, and Export
             </div>
         </CarouselItem>
         <CarouselItem>
-            <div class="demo-carousel">
+            <div class="demo-carousel" style="background-image: linear-gradient( 135deg, #2AFADF 10%, #4C83FF 100%);">
                 Design for Developers
             </div>
         </CarouselItem>
