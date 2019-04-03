83854f3b74fb7fc9d9424c7e531652efcf5dd31f
Bestony <best.tony@foxmail.com>
2019-04-03 22:56:01 +0800

    feat: Index Layout

--- src/views/page/Index.vue
+++ src/views/page/Index.vue
@@ -19,6 +19,27 @@
     margin: 0 auto;
     margin-right: 20px;
 
+.title
+    margin 50px 0px 20px 0px
+
+.logo-matrix
+    margin 20px 0px
+    .logo-container
+        display flex
+        justify-content center
+        align-items center
+        .logo
+            height 100px
+
+.sponsors-matrix
+    margin 20px 0px
+    .sponsor-container
+        display flex
+        justify-content center
+        align-items center
+        .sponsor
+            height 50px
+
 .layout-footer-center
     text-align: center;
 
@@ -51,7 +72,37 @@
             <Content :style="{padding: '50px 50px'}">
 
                <IndexCarousel></IndexCarousel>
-                
+               <div class="title">
+                   <h1>Logos</h1>
+               </div>
+               <div class="logo-matrix">
+                   <Row :gutter="16">
+                        <Col span="6">
+                            <Card :bordered="false" class="logo-container">
+                               <img class="logo" src="@logos/pornhub.png" alt="">
+                            </Card>
+                        </Col>
+                        <Col span="6">
+                            <Card :bordered="false" class="logo-container">
+                               <img class="logo" src="@logos/pornhub-app.png" alt="">
+                            </Card>
+                        </Col>
+                        
+                    </Row>
+               </div>
+               <div class="title">
+                   <h1>Sponsors</h1>
+               </div>
+               <div class="sponsors-matrix">
+                   <Row :gutter="16">
+                        <Col span="6" class="sponsor-container">
+                            <img class="sponsor" src="@/assets/sponsors/linuxcn.jpg" alt="">
+                        </Col>
+                        <Col span="6" class="sponsor-container">
+                                <img class="sponsor" src="@/assets/sponsors/leancloud.png" alt="">
+                        </Col>
+                    </Row>
+               </div>
             </Content>
             <Footer class="layout-footer-center">2019 &copy; Bestony</Footer>
         </Layout>
