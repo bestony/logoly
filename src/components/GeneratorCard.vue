ae9e2239afa3a41ab74fb4a3b8a845294b4f03fc
Bestony <best.tony@foxmail.com>
2019-05-04 09:05:40 +0800

    :lipstick: done Home Card List

---
+++ src/components/GeneratorCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-flex xs12 md6 xl3>
+            <v-card class="mb-2 mx-2">
+              <v-img :src="image" aspect-ratio="2"></v-img>
+
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">{{title?title:'No Title'}}</h3>
+                  <div>{{description?description:'No Description'}}</div>
+                </div>
+              </v-card-title>
+
+              <v-card-actions>
+                <v-btn flat color="orange" :to="to">Try it</v-btn>
+                <v-btn flat color="orange" :to="inspiredPath">Get Inspired</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+</template>
+
+<script>
+export default {
+    props:{
+        image:String,
+        to:String,
+        mode:String,
+        description: String,
+        title: String,
+    },
+    computed:{
+        inspiredPath(){
+            return "/" + this.mode
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,39 @@
 <template>
-    <div id="home">
-        <Carousel></Carousel>
+  <div id="home">
+    <Carousel></Carousel>
+
+    <div class="list">
+      <h3 class="display-2 my-4 font-weight-bold text-uppercase text-xs-center">Generators</h3>
+      <v-container fluid grid-list-xs>
+        <v-layout row wrap>
+          <GeneratorCard 
+            image="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            to="/generator/pornhub"
+            mode="pornhub"
+            title="P*rnhub Style"
+            description="P*rnHub Style Logo Generator"
+            ></GeneratorCard>
+          <GeneratorCard 
+            image="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            to="/generator/pornhub-app"
+            mode="pornhub"
+            title="P*rnhub App Style"
+            description="P*rnHub Style Logo Generator"
+            ></GeneratorCard>
+
+        </v-layout>
+      </v-container>
     </div>
+  </div>
 </template>
 
 <script>
 import Carousel from '@/components/Carousel.vue'
+import GeneratorCard from '@/components/GeneratorCard.vue'
 export default {
-    components:{
-        Carousel
-    }
+  components: {
+    Carousel,
+    GeneratorCard
+  },
 }
 </script>
