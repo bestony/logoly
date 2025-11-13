0a6c24fdd8d6b960723135e67425891f57939dca
bestony <bestony@linux.com>
2025-11-13 21:37:47 +0800

    feat: update dependencies and enhance Vite configuration
    - Added `stylus` and `vite-plugin-compression` to package.json for improved styling and compression capabilities.
    - Configured Vite to use `vite-plugin-compression` with Brotli and Gzip algorithms for better asset optimization.
    - Updated build settings to manage chunking and source map generation effectively.

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,48 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 10240,
+    }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 10240,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    sourcemap: false,
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("vuetify")) return "vuetify";
+            if (id.includes("vue")) return "vue";
+            return "vendor";
+          }
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+      },
+    },
+    esbuild: {
+      drop: ["console", "debugger"],
     },
   },
 });
