c68ca2a5fcb6764eccc20161bf71339f6763da0c
bestony <bestony@linux.com>
2025-11-13 21:04:38 +0800

    fix: update copyright link to new website

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="text-center text-[#666]">
     © Bestony 2019
-    <a href="https://bitbear.net/?refcode=1ndyezv73" target="_blank">比特熊,微服务</a>
+    <a href="https://www.ixiqin.com" target="_blank">website</a>
   </p>
 </template>
