888d4c32ac41df3837f5cb295df267ca4371c52e
bestony <bestony@linux.com>
2025-11-20 09:25:31 +0800

    chore: add @unocss/reset for CSS normalization, reorganize imports in uno.config.ts, and update package.json and package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import 'virtual:uno.css'
+import '@unocss/reset/normalize.css'
 import App from './App.vue'
 import router from './router'
 
