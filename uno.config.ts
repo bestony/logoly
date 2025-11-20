539653668576d02f3955db55b24a0f31ff79c0a8
bestony <bestony@linux.com>
2025-11-20 13:15:43 +0800

    feat: add @iconify-json/mingcute dependency and integrate preset-icons in uno.config.ts for enhanced icon support

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,12 @@
+import presetIcons from '@unocss/preset-icons'
 import presetWind4 from '@unocss/preset-wind4'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
   presets: [
+    presetIcons({
+      /* options */
+    }),
     presetWind4({
       preflights: {
         reset: true,
