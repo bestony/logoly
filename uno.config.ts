18cdc76021b6d0cebba0f43a8041025e572ba07b
bestony <bestony@linux.com>
2025-11-20 09:24:01 +0800

    chore: add UnoCSS presets for enhanced styling capabilities and update package dependencies

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,16 @@
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
+import presetRemToPx from '@unocss/preset-rem-to-px'
+import presetTypography from '@unocss/preset-typography'
+import presetWebFonts from '@unocss/preset-web-fonts'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetIcons(),
+    presetWebFonts(),
+    presetTypography(),
+    presetRemToPx(),
+    presetAttributify(),
+  ],
 })
